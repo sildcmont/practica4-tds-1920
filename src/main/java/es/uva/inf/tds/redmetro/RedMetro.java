be267139b57d8e02a343d7e558d9046fd974a094
Silvia Montero <silviacarmen.montero@alumnos.uva.es>
2020-01-05 21:36:47 +0100

    Creado test del constructor de RedMetro y los stub correspondientes

--- src/main/java/es/uva/inf/tds/redmetro/RedMetro.java
+++ src/main/java/es/uva/inf/tds/redmetro/RedMetro.java
@@ -1,5 +1,32 @@
 package es.uva.inf.tds.redmetro;
 
+import java.util.ArrayList;
+
+/**
+ * Clase que representa una Red de Metro compuesta por Líneas.
+ * Una red de metro debe tener al menos 2 líneas.
+ * @author silmont
+ * @Invariant {@code getLineas().size >= 2}
+ */
+
 public class RedMetro {
+	
+	/**
+	 * Crea un objeto que representa a una red de metro que contiene varias líneas.
+	 * 
+	 * @param lineas
+	 */
+	public RedMetro(Linea...lineas) {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * Consulta las lineas que contiene la red de metro.
+	 * @return lista con las lineas de la red
+	 */
+	public ArrayList<Linea> getLineas() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
